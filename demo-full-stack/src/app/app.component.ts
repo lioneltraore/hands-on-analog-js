bc9eb6993e189c6d2b3655fadfd032bca796f11e
Lionel Traore <hello@lioneltraore.com>
2024-09-16 05:54:40 +0000

    [DEMO FULLSTACK] add header navigation

--- demo-full-stack/src/app/app.component.ts
+++ demo-full-stack/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'demo-full-stack-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <router-outlet></router-outlet> `,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+  <header class="flex p-4 bg-gray-600 w-screen jutify-center text-gray-200 uppercase">
+    <a class="block mr-5" routerLink="/">Home</a>
+    <a class="block mr-5" routerLink="/about">About</a>
+    <a routerLink="/articles">Articles</a>
+  </header>
+  <router-outlet></router-outlet>
+   `,
 })
 export class AppComponent {}
