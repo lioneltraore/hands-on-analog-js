cf2c482d41cf2d277475c621a8a000f93a52a105
Lionel Traore <hello@lioneltraore.com>
2024-09-16 07:52:22 +0000

    [DEMO FULLSTACK] save changes

--- demo-full-stack/src/app/app.component.ts
+++ demo-full-stack/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'demo-full-stack-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   template: `
   <header class="flex p-4 bg-gray-600 w-screen jutify-center text-gray-200 uppercase">
-    <a class="block mr-5" routerLink="/">Home</a>
-    <a class="block mr-5" routerLink="/about">About</a>
-    <a routerLink="/articles">Articles</a>
+    <a class="block mr-5" [routerLink]="'/'" [routerLinkActiveOptions]="{ exact: true }" routerLinkActive="active-header-link">Home</a>
+    <a class="block mr-5" [routerLink]="'/about'" routerLinkActive="active-header-link">About</a>
+    <a [routerLink]="'/articles'" routerLinkActive="active-header-link">Articles</a>
   </header>
   <router-outlet></router-outlet>
    `,
